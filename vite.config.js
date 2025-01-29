539d7000e2dcb92c0bbfda7520c9869106524ea6
Ishtiak13 <ishtiakalhumaidi@gmail.com>
2025-01-29 23:51:27 +0600

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/legal-solution2",
+  base: "/legal-solution2/",
 });
